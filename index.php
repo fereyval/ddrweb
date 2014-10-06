95f6d6da93e110bc01b7fd45a51977f6defb54ec
valentin <vferey@pixellweb.com>
2014-10-06 15:46:12 -0400

    a

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ include("include/entete.php");
                 <div id="slide" class="slide" >
 					<img src="composants/slider-1.jpg"  width="1080" height="478"  style="z-index:-1"/> 
 					<img src="composants/slider-2.jpg"  width="1080" height="478" />
-					<img src="composants/slider-3.jpg"  width="1080" height="478" />
+					<img src="composants/slider-4.jpg"  width="1080" height="478" />
                 </div>
          </div>
 	</div><!-- Fin slide //-->
@@ -88,7 +88,7 @@ include("include/entete.php");
                     Cette sociétée propose différents services en rapport avec la conception de sites internet, que ce soit un simple blog wordpress, un site personnel ou même un E-commerce (simplifié). La chartre graphique, l'intégration et le développement de chaque sites web sont fait par moi. Pour plus d'informations sur les services que je propose, cliquez sur le bouton ci-dessous.
                     </p>
                 </div>
-				 <p class="txtcenter mbm right mrl mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
+				 <p class="txtcenter mbm left mll mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
 			</div>
 			<div class="col w50 ptm">
 			<img src="styles/qui.png" class="left">
@@ -99,69 +99,78 @@ include("include/entete.php");
                     Je suis un étudiant de 2ème année à Epitech lyon, une école d'informatique qui propose un cursus d'expert en 5 ans durant lequel vous serez confronté à faire environ 1 an et demi de stage en entreprise.
                     </p>
                     <p >
-                    Je code depuis près de 3 ans et ai appris les bases du Web grâce à l'association Epiweb Lyon et me suis formé durant un stage de 4 mois chez Pixellweb une entreprise également basée sur la conception de sites internet aux Antilles. Durant ces 3 années à coder, j'ai aquis pas mal de compétences que vous pourrez décortiquer en cliquant sur le lien suivant.
+                    Je code depuis près de 3 ans et ai appris les bases du Web grâce à l'association Epiweb Lyon et me suis formé durant un stage de 4 mois chez Pixellweb une entreprise également basée sur la conception de sites internet aux Antilles. Pour en savoir plus sur mes compétences, cliquez sur le lien ci-dessous.
                     </p>
                 </div>
-				<p class="txtcenter mbm right mrl mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
+				<p class="txtcenter mbm left mll mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
 			</div>
 		</div>
-		<div class="ptl mtl">
-			<h2 class="title-h2" id="rea"><span>Mes dernières</span> réalisations</h2>
+        <div class="row pts pll">
+			<div class="col w70 pam">
+                <h2 class="title-h2"><span>Nos</span> Services</h2>
+                <div class="box5">
+                    <img src="composants/img1.jpg" alt="developpement" class="products"/>
+                        <div class="left w90">
+                        <h3 class="title-h3"><span>Formule Simple</span></h3>
+                    <p style="margin-top: 0px;">La formule basique à petits prix et très rapide!</p>
+                    </div>
+                </div>
+                <div class="box5">
+                    <img src="composants/img2.jpg" alt="developpement" class="products"/>
+                    <div class="left w90">
+                        <h3 class="title-h3"><span>Formule Premium</span></h3>
+                        <p style="margin-top: 0px;">La formule all inclusive, pour créer un site à votre image dans un délais imposé.</p>
+                    </div>
+                </div>
+                <div class="box5">
+                    <img src="composants/img3.jpg" alt="developpement" class="products"/>
+                    <div class="left w90">
+                        <h3 class="title-h3"><span>Abonnement DDRWeb</span></h3>
+                        <p style="margin-top: 0px;">Un abonnement au mois qui permet un suivi et un entretien continu de votre site</p>
+                    </div>
+                </div>
+                <p class="txtcenter mbm left mls mtl ptl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
+            </div>
+			<div class="col w30 ptm prm">
+                <h2 class="title-h2"><span>B</span>log</h2>
+                <div class="mts">
+                    <div class="date"><strong>05</strong><span>October, 2014</span></div>
+                    <p><a href="#" class="link">Lorem ipsum dolor sit amet consetetur</a><br>Sadipscing elitriam nonumy eirmod nonumy eirmod tempor invidunt labore.</p>
+                </div>
+                <div class="mts">
+                    <div class="date"><strong>06</strong><span>October, 2014</span></div>
+                    <p><a href="#" class="link">Lorem ipsum dolor sit amet consetetur</a><br>Sadipscing elitriam nonumy eirmod nonumy eirmod tempor invidunt labore.</p>
+                </div>
+                <p class="txtcenter mbm left mls mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>
+            </div>
+        </div>
+		<div class="mts">
+			<h2 class="title-h2 txtcenter" id="rea"><span>Mes dernières</span> réalisations</h2>
 			<div id="carousel">
 			<div class="carousel-feature">
 				<a href="sites/bijoudexpo/index.html"><img class="carousel-image" alt="Image Caption" src="styles/last/bijoudexpo.jpg"></a>
-				<div class="carousel-caption">
-					<p>Un bijou d'expo : Ce site à été fait dans le cadre d'une recherche de stage. Il présente une exposition de bijou ayant eu lieu à Reims le 18/06/14.</p>
-				</div>
+				
 			</div>
 			<div class="carousel-feature">
 				<a href="http://antilles-recouvrement.com/"><img class="carousel-image" alt="Image Caption" src="styles/last/antillerecou.jpg"></a>
-				<div class="carousel-caption">
-					<p>Antille Recouvrement : Site réalisé pendant mon stage de 4 mois chez Pixell Martinique.</p>
-				</div>
+				
 			</div>
 			<div class="carousel-feature">
 				<a href="#"><img class="carousel-image" alt="Image Caption" src="styles/last/ddrweb.jpg"></a>
-				<div class="carousel-caption">
-					<p>DDR Web : C'est le site sur lequel vous êtes il fut créer par moi et moi seul pendant ma periode de stage.</p>
-				</div>
+				
 			</div>
 			<div class="carousel-feature">
 				<a href="http://inkcaraibes-mq.com/"><img class="carousel-image" alt="Image Caption" src="styles/last/ink.jpg"></a>
-				<div class="carousel-caption">
-					<p>Ink'martinique : Site réalisé pendant mon stage de 4 mois chez Pixell Martinique.</p>
-				</div>
+				
 			</div>
 			<div class="carousel-feature">
 				<a href="404.hmtl"><img class="carousel-image" alt="Image Caption" src="styles/last/vide.jpg"></a>
-				<div class="carousel-caption">
-					<p>Emplacement vide : Devenez client pour que votre site apparaisse ici!</p>
-				</div>
+				
 			  </div>
 			</div>
+            <!--<p class="txtcenter mbm left mll pll mtl"><img src="styles/button3.png"><a style="color:#FF9000" class="bouton1" href="#">Lire la suite</a></p>-->
 		</div>
 	</div>	
 </div>
 
-
-<div role="main" class="texte6 pam">
-	<div class="conteneur center">
-		<div class="accordion" id="for">
-            <img src="styles/button.png" class="pad2"/>
-			<p class="mbm mll fuck accordion-titre"><a style="color:#FFFFFF" class="bouton2"><img src="styles/btn.png" id="test" onclick="change();" class="pad"/>Pour démarrer un contrat, cliquez ici</a></p>
-			<div class="formulaire1">
-				<form method="post" action="contact.php">
-					<p class="form-style" >Nom : <input type="text" name="nom" /></p>
-				</form><br>
-				<form method="post" action="contact.php">
-					<p class="form-style">Prénom : <input type="text" name="prenom" /></p>
-				</form><br>
-				<form method="post" action="contact.php">
-					<p class="form-style">E-mail : <input type="email" placeholder="Ex : test@hotmail.fr" name="email" /></p>
-				</form><br>
-				<a href="conditions.php"><input type="submit" value="Continuer" /></a>
-			</div>
-		</div>
-	</div>
-</div>
 <?php include("include/baspage.php"); ?>
